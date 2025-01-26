1572f8b4fe3019b4b651e6c267d699dce1c0ede0
OhFrancy <ohfrancy@gmail.com>
2025-01-27 00:20:09 +0100

    #2 Added a feature to disable caps lock, one time, if enabled beforehand

--- src/caps_script.sh
+++ src/caps_script.sh
@@ -2,6 +2,7 @@
 
 # Saves Process ID to kill it later
 CURRENT_PID=$$
+COUNT=0
 
 # Sets caps lock to not active
 xmodmap -e "keycode 66 = NoSymbol"
@@ -13,7 +14,18 @@ while true; do
      if [ ! $YES -eq 0 ]; then
           break
      fi
-     sleep 1
+     # Checks if Caps lock is turned on, if it is, re-enable it and wait for the user to turn it off
+     if [ $COUNT -eq 0 ] && xset -q | grep -o "Caps Lock:   on" > /dev/null 2>&1; then
+          xmodmap -e "keycode 66 = Caps_Lock"
+     elif [ $COUNT -eq 0 ] && ! xset -q | grep -o "Caps Lock:   on" > /dev/null 2>&1; then
+          xmodmap -e "keycode 66 = NoSymbol"
+          COUNT=1
+     fi
+     if [ $COUNT -eq 0 ]; then
+          sleep 0.3
+     else
+          sleep 1
+     fi
 done
 
 # Sets the Caps Lock back to active and kills the process
--- src/inst.sh
+++ src/inst.sh
@@ -3,26 +3,40 @@
 DEPENDENCY=x11-xserver-utils
 SCRIPT=caps_script.sh
 TARGET=caps_script
-DIRECTORY=Nvim-Caps-Disabler
+DIR=Nvim-Caps-Disabler
 
-if ! dpkg -s $DEPENDENCY > /dev/null 2>&1; then
-     echo "$DEPENDENCY not installed, install it and try again."
-elif [ ! -f $SCRIPT ]; then
+if [ ! -f $SCRIPT ]; then
      echo "$SCRIPT not found, make sure that it's in the right folder."
 else
+     if ! dpkg -s $DEPENDENCY > /dev/null 2>&1; then
+          echo "$DEPENDENCY not installed, proceeding to install it..."
+          sudo apt install $DEPENDENCY
+     fi
+
      echo "Making the script an executable..."
-     chmod a+x $SCRIPT > /dev/null 2>&1
-     mv $SCRIPT $TARGET > /dev/null 2>&1
+     if ! chmod a+x $SCRIPT > /dev/null 2>&1; then
+         echo "Couldn't run 'chmod a+x', not enough permissions, exiting..."
+         exit
+     fi
+     if ! mv $SCRIPT $TARGET > /dev/null 2>&1; then
+          echo "Couldn't run 'mv $SCRIPT $TARGET', not enough permissions, exiting"
+          exit
+     fi
      echo "Successful."
      sleep 0.5
      echo "Do you want to move the script to the desired location? [Y/n]"
      read prompt
      case $prompt in
           y | Y)
-               echo "Moving the script to ~/$DIRECTORY."
-               mkdir /home/$USER/$DIRECTORY
-               mv $TARGET /home/$USER/$DIRECTORY/
-               echo "Installation completed"
+               echo "Moving the script to ~/$DIR."
+               if [ -d /home/$USER/$DIR ]; then
+                    echo "Directory already exists..."
+               else
+                    mkdir /home/$USER/$DIR > /dev/null 2>&1
+                    echo "Directory Successfully created..."
+               fi
+               mv $TARGET /home/$USER/$DIR > /dev/null 2>&1
+               echo "Installation completed."
                ;;
           *)
                echo "Installation completed, run it with './caps_script &'"
